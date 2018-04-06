3b02d87a902cdee74b68b309a765b7e50fcff68f
saahilclaypool <saahil@claypools.org>
2018-04-05 20:19:26 -0400

    started some busy work

--- Prototype/src/App.js
+++ Prototype/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Interactive } from './Interactive'; 
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Interactive
+          has_condition={0.1}
+          positive_condition={0.9}
+          positive_no_condition={0.1}
+          number_of_people={20}
+          test={0}
+          condition={0}
+        />
       </div>
     );
   }
---
+++ Prototype/src/Interactive.js
@@ -0,0 +1,206 @@
+import React, { Component } from 'react';
+import './Interactive.css';
+import * as d3 from 'd3'; 
+
+export class Interactive extends Component {
+  d3id = "D3Vis";
+
+  defaults = {
+    height: 600, width: 600, margin : {
+      top: 20, right: 20, bottom: 20, left: 250,
+    }
+  }
+  /**
+   * props: 
+   *  has_condition: float
+   *  positive_condition: float
+   *  positive_no_condition: float
+   *  number_of_people: count
+   *  test: -1, 0, 1: 0 if not known
+   *  condition: -1, 0, 1: 0 if not known
+   *  
+   */
+  constructor(props) {
+    super(props); 
+  }
+  componentDidMount() {
+    this.renderD3(); 
+  }
+
+  componentDidUpdate() {
+    this.renderD3(); 
+  }
+
+  renderD3 = () => {
+    let chartWidth = this.defaults.height - this.defaults.margin.left - this.defaults.margin.right; 
+    let chartHeight = this.defaults.height - this.defaults.margin.top - this.defaults.margin.bottom; 
+    let containerId = '#' + this.d3id; 
+
+    
+
+    d3.select(containerId).html(''); // just clear out what is there
+
+    var chart = d3.select(containerId).append('svg')
+      .attr('width', this.defaults.width)
+      .attr('height', this.defaults.height)
+      .attr('id', 'chart')
+      .append('g')
+        .attr('transform', `translate(${this.defaults.margin.left}, ${this.defaults.margin.top})`);
+
+
+    // set up axis
+    let data = this.make_data(); 
+    console.log(data);
+
+    // for each label, put it on the left of the line (using margin)
+
+    let labelHeight = chartHeight / 4; 
+
+    let attributes = chart.selectAll('.attribute')
+      .data(data)
+      .enter() 
+      .append('g')
+      .attr('transform', (_, i) => {
+        return `translate(-10, ${labelHeight * (i + .5)})`;
+      });
+      attributes.append('text')
+      .attr('text-anchor', 'end')
+      .text((d, i) => d.label);
+      attributes.append('circle')
+        .attr('cx', (d, i) => i * 10)
+        .attr('r', 10)
+        .attr('fill', 'black');
+
+
+    chart.append('line')
+      .attr('x1', '0')
+      .attr('x2', '0')
+      .attr('y1', '0')
+      .attr('y2', chartHeight)
+      .attr('stroke-width', 2)
+      .attr('stroke', 'black');
+  }
+
+  make_data = () => {
+    let number = this.props.number_of_people;
+    let has_condition = Math.round(this.props.has_condition * number),
+        no_condition = Math.round((1 - this.props.has_condition) * number),
+        positive_condition = Math.round(this.props.positive_condition * has_condition), 
+        negative_condition = Math.round((1 - this.props.positive_condition) * has_condition), 
+        positive_no_condition = Math.round(this.props.positive_no_condition * no_condition), 
+        negative_no_condition = Math.round((1 - this.props.positive_no_condition) * no_condition);
+      
+    console.log("has condition", has_condition)
+    console.log("no condition", no_condition)
+    console.log("pos, has condition", positive_condition)
+    console.log("negative, has condition", negative_condition)
+    console.log("pos, no condition", positive_no_condition)
+    console.log("neg, no condition" , negative_no_condition)
+    let data  = [], labels = []; 
+    if(this.props.test === 0) {
+      if(this.props.condition === 0) {
+        labels = [
+          "Positive Test & Has Condition",
+          "Positive Test & No Condition",
+          "Negative Test & Has Condition",
+          "Negative Test & No Condition", 
+        ]; 
+        data = [
+          positive_condition, 
+          positive_no_condition, 
+          negative_condition, 
+          negative_no_condition
+        ]
+      } else if (this.props.condition === 1) {
+        labels = [
+          "Positive Test & Has Condition",
+          "Negative Test & Has Condition",
+        ]; 
+        data = [
+          positive_condition, 
+          negative_condition, 
+        ]
+      } else {
+        labels = [
+          "Positive Test & No Condition",
+          "Negative Test & No Condition", 
+        ]; 
+        data = [
+          positive_no_condition, 
+          negative_no_condition
+        ]
+      }
+    } else if (this.props.test == 1) {
+      if(this.props.condition === 0) {
+        labels = [
+          "Positive Test & Has Condition",
+          "Positive Test & No Condition",
+        ]; 
+        data = [
+          positive_condition, 
+          positive_no_condition, 
+        ]
+      } else if (this.props.condition === 1) {
+        labels = [
+          "Positive Test & Has Condition",
+        ]; 
+        data = [
+          positive_condition, 
+        ]
+      } else {
+        labels = [
+          "Positive Test & No Condition",
+        ]; 
+        data = [
+          positive_no_condition, 
+        ]
+      }
+
+    } else {
+      if(this.props.condition === 0) {
+        labels = [
+          "Negative Test & Has Condition",
+          "Negative Test & No Condition", 
+        ]; 
+        data = [
+          negative_condition, 
+          negative_no_condition
+        ]
+      } else if (this.props.condition === 1) {
+        labels = [
+          "Negative Test & Has Condition",
+        ]; 
+        data = [
+          negative_condition, 
+        ]
+      } else {
+        labels = [
+          "Negative Test & No Condition", 
+        ]; 
+        data = [
+          negative_no_condition
+        ]
+      }
+    }
+
+    let d = [];
+    for (var i = 0; i < labels.length; i++) {
+      d.push({label: labels[i], count: data[i]})
+    }
+
+    return d;
+
+  }
+
+  /**
+   * This just creates a div + element to hold d3 svg things
+   */
+  render() {
+    return (
+        <div className="Interactive">
+          <h2>Total: {this.props.number_of_people}</h2>
+          <div id={this.d3id}/>
+        </div>
+    );
+  }
+}
